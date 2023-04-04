167f0c2ec6c2c6d3dc13794c3d8e7be653bdc869
darshan <43618707+mevadadarshan19@users.noreply.github.com>
2023-04-04 12:21:29 +0530

    Update bliss_X01BD.mk

--- bliss_X01BD.mk
+++ bliss_X01BD.mk
@@ -16,7 +16,7 @@ $(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
 #Sony Dolby
-$(call inherit-product, vendor/dolby/config.mk)
+#$(call inherit-product, vendor/dolby/config.mk)
 
 # Inherit from X01BD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
