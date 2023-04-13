bed32f29d47767d3ea611a57b9eeb9885b9dd03c
darshan <43618707+mevadadarshan19@users.noreply.github.com>
2023-04-13 16:10:03 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/bootleg_X01BD.mk
+    $(LOCAL_DIR)/derp_X01BD.mk
 
 COMMON_LUNCH_CHOICES := \
-    bootleg_X01BD-eng \
-    bootleg_X01BD-user \
-    bootleg_X01BD-userdebug
+    derp_X01BD-eng \
+    derp_X01BD-user \
+    derp_X01BD-userdebug
